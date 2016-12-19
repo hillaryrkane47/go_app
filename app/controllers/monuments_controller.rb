9c0e83cf5f3c1431960ddac26b7b2729b0cfb8fc
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-19 09:40:58 -0500

    tester file

--- app/controllers/monuments_controller.rb
+++ app/controllers/monuments_controller.rb
@@ -1,2 +1,12 @@
 class MonumentsController < ApplicationController
+
+  def index
+    @monuments = Monument.all
+  end
+
+  def show
+    @monument = Monument.find_by(id: params[:id])
+  end
+
+
 end
