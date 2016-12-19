239f5caa7545efde93b5b2999b36e68fb6e4ea3b
Danielle Twaalfhoven <danielle12hoven@gmail.com>
2016-12-19 10:01:48 -0500

    testing branches

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,21 @@ class HomeController < ApplicationController
   # if user_signed_in?
     # get their id with the current_user method
     # Then do @monuments/@trips = Trips.where (user.id = id etc etc)
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+
+def create
+    User.create(email: params[:Email],
+                  )
+    redirect_to "/home"
+  end
+
+
 end
