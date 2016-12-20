c0e314658ecb9b90a0e6e2f8906e4fc12fbbfbfc
Hillary Kane <hillaryrkane47@gmail.com>
2016-12-20 12:32:24 -0500

    added button and search page rendering api results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,19 +5,33 @@ class HomeController < ApplicationController
     # Then do @monuments/@trips = Trips.where (user.id = id etc etc)
 
   def index
-    @users = User.all
+    @monuments = Monument.all
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    @monument = Monument.find(params[:id])
   end
 
+  def create
+    home = params['home']
+    city = home['city']
+    @url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=monuments+in+"+city+"&key=AIzaSyAaTxKO83nQMSzEMq5T-WqU9thoXryHcaM"
+    @response = HTTParty.get(@url)
+    mon_name = params[:mon][:monu]
+    address = params[:mon][:address]
+    pic = params[:mon][:pic]
+    puts mon_name, address, pic
+    render 'new'
+  end
+  def new
+    @response
+  end
+  def update
 
-def create
-    User.create(email: params[:Email],
-                  )
-    redirect_to "/home"
   end
 
+  def destroy
+
+  end
 
 end
